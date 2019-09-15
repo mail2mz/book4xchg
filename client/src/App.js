37294e070f990032ce90d4344746ef2319716d8b
root <root@Linda.localdomain>
2019-09-15 13:03:20 -0400

    remove proxy

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
   // our first get method that uses our backend api to
   // fetch data from our data base
   getDataFromDb = () => {
-    fetch('/api/getData')
+    fetch('http://localhost:3001/api/getData')
       .then((data) => data.json())
       .then((res) => this.setState({ data: res.data }));
   };
@@ -56,7 +56,7 @@ class App extends Component {
       ++idToBeAdded;
     }
 
-    axios.post('/api/putData', {
+    axios.post('http://localhost:3001/api/putData', {
       id: idToBeAdded,
       message: message,
     });
@@ -73,7 +73,7 @@ class App extends Component {
       }
     });
 
-    axios.delete('/api/deleteData', {
+    axios.delete('http://localhost:3001/api/deleteData', {
       data: {
         id: objIdToDelete,
       },
